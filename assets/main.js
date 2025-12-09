9b368b9d1fbe6f68cb9d095f6f531f8e8c89958b
Irma López <ada_wit18@hackademy.mx>
2025-12-09 13:17:46 -0600

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
-const url = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC_VSuoWdRWt2Rea6vDQrKcA&filter=videos_latest&hl=en&gl=US';
+const url = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC_VSuoWdRWt2Rea6vDQrKcA&part=snippet%2Cid&order=date&maxResults=50';
 
 const content = null || document.getElementById('content');
 console.log(content);
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '60aaa775f0mshdbbeba2d24d3fdcp133748jsnd33128d90d1c',
-		'x-rapidapi-host': 'youtube138.p.rapidapi.com'
+    'x-rapidapi-key': '60aaa775f0mshdbbeba2d24d3fdcp133748jsnd33128d90d1c',
+    'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
 	}
 };
 
@@ -18,24 +18,43 @@ async function fetchData(urlApi) {
 (async () => {
     try {
         const videos = await fetchData(url);
-        videos.contents.map(video => console.log(video.video.thumbnails[0].url));
+        videos.items.map(video => console.log(video.snippet.thumbnails.high.url));
         let view = `
-        ${videos.contents.map(video => `
+        ${videos.items.map(video => `
         <div class="group relative">
           <div
-         <div class="group relative">
-          <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.video.thumbnails[0].url}" alt="${video.video.title}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.title}" class="w-full">
           </div>
           <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
               <span aria-hidden="true" class="absolute inset-0"></span>
-              ${video.video.title}
+              ${video.snippet.title}
             </h3>
           </div>
-        </div>`).slice(0, 4).join('')}
+        </div>`).slice(0, 20).join('')}
         `;
+        // const videos = await fetchData(url);
+
+        // const view = videos.items
+        //   .slice(0, 12) // o los que quieras mostrar
+        //   .map(video => `
+        //     <div class="group relative">
+        //       <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+        //         <img src="${video.snippet.thumbnails.high.url}" 
+        //             alt="${video.snippet.title}" 
+        //             class="h-full w-full object-cover">
+        //       </div>
+
+        //       <div class="mt-4">
+        //         <h3 class="text-sm text-gray-700">${video.snippet.title}</h3>
+        //       </div>
+        //     </div>
+        //   `)
+        //   .join("");
+
+        // document.querySelector("#content").innerHTML = view;
+
         console.log(view);
         content.innerHTML = view;
     } catch (error) {
